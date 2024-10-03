c33a4c241d4de8857b07697a4887f80e3b95daab
Nawneet Raj <114335800+Nawneetbth@users.noreply.github.com>
2024-10-03 12:12:09 +0530

    Add files via upload

---
+++ memory_management.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+void firstFit(int blockSize[], int m, int processSize[], int n) {
+    int allocation[10];
+
+    for (int i = 0; i < n; i++) {
+        allocation[i] = -1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (blockSize[j] >= processSize[i]) {
+                allocation[i] = j;
+                blockSize[j] -= processSize[i];
+                break;
+            }
+        }
+    }
+
+    printf("\nProcess No.\tProcess Size\tBlock No.\n");
+    for (int i = 0; i < n; i++) {
+        printf(" %d\t\t%d\t\t", i + 1, processSize[i]);
+        if (allocation[i] != -1)
+            printf("%d\n", allocation[i] + 1);
+        else
+            printf("Not Allocated\n");
+    }
+}
+
+int main() {
+    int blockSize[10], processSize[10];
+    int m, n;
+
+    printf("Enter the number of memory blocks: ");
+    scanf("%d", &m);
+    printf("Enter the sizes of the blocks:\n");
+    for (int i = 0; i < m; i++) {
+        scanf("%d", &blockSize[i]);
+    }
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    printf("Enter the sizes of the processes:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &processSize[i]);
+    }
+
+    firstFit(blockSize, m, processSize, n);
+
+    return 0;
+}
